2c67e663fd92c5d0c17f901c4426d8385db9745d
carlosdos818@yahoo.com <carlosdos818@yahoo.com>
2023-09-18 18:32:18 -0400

    admin can add feedings to finches

--- main_app/models.py
+++ main_app/models.py
@@ -2,6 +2,13 @@ from django.db import models
 # need the reverse method from django urls for redirects
 from django.urls import reverse
 
+# A tuple of 2-tuples
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
+
 # Create your models here.
 class Finch(models.Model):
     name = models.CharField(max_length=100)
@@ -16,3 +23,20 @@ class Finch(models.Model):
     # kwargs are unknown number of arguments
     def get_absolute_url(self):
         return reverse('detail', kwargs={'finch_id': self.id})
+    
+
+    # Model for Feeding (Finch -|---< Feeding)
+class Feeding(models.Model):
+    date = models.DateField('feeding date')
+    meal = models.CharField(
+        max_length=1,
+        choices=MEALS,
+        default=MEALS[0][0]
+    )
+    # creates finch FK reference
+    finch = models.ForeignKey(Finch, on_delete=models.CASCADE)
+    
+
+    def __str__(self):
+        # Use the 'meal' attribute directly to get the meal name
+        return f"{self.get_meal_display()} on {self.date}"
